7254992f5bb597922645aab2642521900f35b25b
Marwan-Elsherif <94860269+Marwan-Elsherif@users.noreply.github.com>
2022-06-13 09:57:46 -0700

    Time: 0 ms (100.00%), Space: 54.2 MB (47.65%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, 0, nums.length-1, target);
+    }
+    
+    
+    int binarySearch(int[] arr, int l, int r, int target){
+        if(r>=l){
+            int mid = l + (r - l) / 2;
+            if(arr[mid] == target) return mid;
+            if(arr[mid] > target) return binarySearch(arr, l, mid-1, target);
+            return binarySearch(arr, mid+1 , r, target);
+        }
+        return -1;
+    }
+}
