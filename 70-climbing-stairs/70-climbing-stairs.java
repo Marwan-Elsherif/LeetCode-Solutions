13a94a195d798fed840c977f0ec2b5853eb3b9f7
Marwan-Elsherif <94860269+Marwan-Elsherif@users.noreply.github.com>
2022-09-09 16:27:20 -0700

    Time: 0 ms (100.00%), Space: 40.8 MB (59.48%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -4,7 +4,11 @@ class Solution {
         for (int i = 0; i < n + 1; i++) {
           dp[i] = -1;
         }
-        getStairs(n, dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n ; i++) {
+          dp[i] = dp[i-1]+ dp[i-2];
+        }
         return dp[n];   
     }
     private int getStairs(int n, int dp[]){
